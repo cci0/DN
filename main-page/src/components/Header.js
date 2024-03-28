ea3904dc7ce2a291f4a49bb21a29403fe9b94828
cci0 <dudtidd4484@gmail.com>
2024-03-28 23:52:51 +0900

    fix: header scss

--- main-page/src/components/Header.js
+++ main-page/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoPng from '../images/Logo2.png';
@@ -12,56 +12,61 @@ import diary from '../icons/book-heart.svg';
 import '../styles/header.scss';
 
 export default function Header() {
+    const [mbSidebar, setmbSidebar] = useState(false);
+    const toggleMbSidebar = () => {
+        setmbSidebar(!mbSidebar);
+    };
+
     return (
         <header className="header">
             <nav className="navBar">
                 {/* 모바일 버전 */}
-
                 <div className="mb-header">
-                    <div>
-                        <Link to={'/'}>
-                            <img className="mb-oddLogo" src={logoPng} alt="Loge" />
-                        </Link>
-                    </div>
-                    <div className="mb-hamburger-menu">
+                    <Link to={'/'}>
+                        <img className="mb-oddLogo" src={logoPng} alt="Loge" />
+                    </Link>
+
+                    <div className="mb-hamburger-menu" onClick={toggleMbSidebar}>
                         <img src={menuSvg} alt="menu" />
                     </div>
                 </div>
-
-                <div className="mb-sidebar">
-                    <ul className="mb-sidebar-menu">
-                        <li>
-                            <Link to={'/Login'}>
-                                <img src={login} alt="Login" />
-                                로그인
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/Signup'}>
-                                <img src={signup} alt="SignUp" />
-                                회원가입
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/Calender'}>
-                                <img src={calendar} alt="Calendar" />
-                                캘린더
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/TodoList'}>
-                                <img src={todo} alt="TodoList" />
-                                Todo List
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/MyDiary'}>
-                                <img src={diary} alt="Diary" />
-                                나의 일기
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                {/* 모바일 사이드 바 */}
+                {mbSidebar && (
+                    <div className="mb-sidebar">
+                        <ul className="mb-sidebar-menu">
+                            <li>
+                                <Link to={'/Login'}>
+                                    <img src={login} alt="Login" />
+                                    <span>로그인</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/Signup'}>
+                                    <img src={signup} alt="SignUp" />
+                                    <span>회원가입</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/Calender'}>
+                                    <img src={calendar} alt="Calendar" />
+                                    <span>캘린더</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/TodoList'}>
+                                    <img src={todo} alt="TodoList" />
+                                    <span>Todo List</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={'/MyDiary'}>
+                                    <img src={diary} alt="Diary" />
+                                    <span>나의 일기장</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </nav>
         </header>
     );
